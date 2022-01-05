247bf919fa136bc5805d0b586901067dbff41cc2
markus <markus@DESKTOP-LSJC73I>
2022-01-05 16:39:38 +0100

    Add project files.

---
+++ ProgressMatrixControl.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace ProgressMatrixLibrary
+{
+    partial class ProgressMatrixControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(101, 101);
+            this.panel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(1);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(101, 101);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(101, 101);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
---
+++ ProgressMatrixControl.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ProgressMatrixLibrary
+{
+
+
+    public partial class ProgressMatrixControl : UserControl
+    {
+
+        public class ProgressSquare : Panel
+        {
+
+            public ProgressSquare()
+            {
+
+                BackColor = Color.White;
+                Dock = DockStyle.Fill;
+                DoubleBuffered = true;
+                BorderStyle = BorderStyle.FixedSingle;
+                Margin = new Padding(1);
+
+
+            }
+           
+
+            public void FadeIn(Color BaseColor)
+            {
+
+                for(int alpha=0; alpha<255;++alpha)
+                {
+                                        
+                    this.BackColor = Color.FromArgb(alpha, BaseColor);
+                    Thread.Sleep(2);
+                }
+
+            }
+            public void FadeOut(Color BaseColor)
+            {
+
+                for (int alpha = 255; alpha> 0; --alpha)
+                {
+
+                    this.BackColor = Color.FromArgb(alpha, BaseColor);
+                    Thread.Sleep(2);
+                }
+
+            }
+
+        }
+      
+
+        CancellationTokenSource tokenSource;
+
+        public ProgressMatrixControl()
+        {
+            
+            InitializeComponent();
+           
+            tokenSource = new CancellationTokenSource();
+            for (int y = 0; y < 3; ++y)
+            {
+                for (int x = 0; x < 3; ++x)
+                {
+
+                    tableLayoutPanel1.Controls.Add(new ProgressSquare(), x, y);
+
+                }
+            }
+           
+        }
+        bool mStop = false;
+        public void Stop()
+        {
+            mStop = true;
+            tokenSource.Cancel();
+        }
+       
+        public void ProgressSimple()
+        {
+          
+            var token = tokenSource.Token;
+
+            var t = Task.Run(() =>
+            {
+
+                if (tableLayoutPanel1 != null)
+                {
+
+                    while (false == mStop)
+                    {
+
+                        for (int y = 2; y >= 0; --y)
+                        {
+                            for (int x = 0; x < 3; ++x)
+                            {
+
+                                ProgressSquare cellSquare = (ProgressSquare)tableLayoutPanel1.Controls[x + 3 * y];
+                                cellSquare.FadeIn(Color.FromArgb(123, 123, 234));
+                                //sq.FadeOut(Color.FromArgb(123, 123, 234));
+                                if (token.IsCancellationRequested) break;
+                            }
+                            Thread.Sleep(2);
+                        }
+                        if (token.IsCancellationRequested) break;
+                    }
+                    
+                        if (token.IsCancellationRequested)   return;
+                   
+                }
+            }, token);
+        }
+
+
+
+
+    }
+   
+}
