d998620ee6bb9865634956af619765b95e3953bf
vlovo <MLeitz@boptics.de>
2022-01-09 00:34:13 +0100

    add more animations

--- ProgressMatrixControl.cs
+++ ProgressMatrixControl.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System.ComponentModel;
+using System.Drawing;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,55 +17,95 @@ namespace ProgressMatrixLibrary
             public ProgressSquare()
             {
 
-                BackColor = Color.White;
+                BackColor = Color.WhiteSmoke;
                 Dock = DockStyle.Fill;
                 DoubleBuffered = true;
-                BorderStyle = BorderStyle.FixedSingle;
+                BorderStyle = BorderStyle.None;
                 Margin = new Padding(1);
-           
+                State = 0;
 
 
             }
-           
 
+            public int State
+            {
+                set; get;
+            }
             public void FadeIn(Color BaseColor)
             {
+                //SolidBrush shadowBrush = new SolidBrush(Color.Red);
 
-                for(int alpha=0; alpha<255;++alpha)
+                for (int alpha = 0; alpha < 255; alpha += 2)
                 {
-                                        
+
                     this.BackColor = Color.FromArgb(alpha, BaseColor);
-                    Thread.Sleep(2);
+                    Thread.Sleep(1);
+
+                    //shadowBrush.Color = Color.FromArgb(alpha, BaseColor);
+                    //Rectangle r = this.ClientRectangle;
+                    //Graphics g =  this.CreateGraphics();
+                    //g.FillRectangle(shadowBrush, r.X, r.Y, r.Width, r.Height);
+
                 }
+                State = 1;
 
             }
             public void FadeOut(Color BaseColor)
             {
 
-                for (int alpha = 255; alpha> 0; --alpha)
+                for (int alpha = 255; alpha > 0; alpha -= 2)
                 {
 
                     this.BackColor = Color.FromArgb(alpha, BaseColor);
-                    Thread.Sleep(2);
+                    Thread.Sleep(1);
                 }
-
+                 BackColor = Color.WhiteSmoke;
+                State = 0;
             }
 
-        }
-      
 
+
+        }
+
+        public enum ProgressStyle
+        {
+            Standard = 0, Animation = 1
+        };
         CancellationTokenSource mTokenSource;
         private Task mProgressTask;
         private Color mCellColor;
+
         public ProgressMatrixControl()
         {
 
             InitializeComponent();
             InitializeMatrix();
-            
+
+
 
         }
 
+        int mValue = 0;
+        [Description(""), Category("Behaviour")]
+        public int Value
+        {
+            get
+            {
+                return mValue;
+            }
+            set
+            {
+                mValue = value;
+
+
+            }
+        }
+        [Description(""), Category("Behaviour")]
+        public ProgressStyle Style
+        {
+            get; set;
+
+        }
         private void InitializeMatrix()
         {
             for (int y = 0; y < 3; ++y)
@@ -79,12 +120,12 @@ namespace ProgressMatrixLibrary
         }
 
         bool mStop = false;
-        public void Stop()
+        public void StopProgress()
         {
             mStop = true;
             mTokenSource.Cancel();
             mProgressTask.Wait();
-            ResetCells(); 
+            ResetCells();
 
         }
 
@@ -96,7 +137,7 @@ namespace ProgressMatrixLibrary
                 {
 
                     ProgressSquare cellSquare = (ProgressSquare)tableLayoutPanel1.Controls[x + 3 * y];
-                    cellSquare.BackColor = Color.White;
+                    cellSquare.BackColor = Color.WhiteSmoke;
 
                 }
             }
@@ -109,39 +150,91 @@ namespace ProgressMatrixLibrary
             mStop = false;
             mCellColor = Color.FromArgb(0, 144, 223);
 
-             mProgressTask = Task.Run(() =>
-            {
-                FadeCells(ref cancelToken);
+            mProgressTask = Task.Run(() =>
+           {
+               //AnimateCellsRowWise(ref cancelToken);
+               //AnimateCellsColumnBlocks(ref cancelToken);
+               AnimateCellsRandom(ref cancelToken);
 
-            }, cancelToken);
+           }, cancelToken);
 
-            
+
         }
 
-        private void FadeCells(ref CancellationToken token)
+        private void AnimateCellsRowWise(ref CancellationToken token)
         {
             while (false == mStop)
             {
 
                 for (int y = 2; y >= 0; --y)
                 {
+                    if (token.IsCancellationRequested) break;
+
                     for (int x = 0; x < 3; ++x)
                     {
 
                         ProgressSquare cellSquare = (ProgressSquare)tableLayoutPanel1.Controls[x + 3 * y];
                         cellSquare.FadeIn(mCellColor);
-                        if (token.IsCancellationRequested) break;
+
+
                     }
-                    Thread.Sleep(2);
-                    if (token.IsCancellationRequested) break;
+
+
                 }
                 ResetCells();
 
 
             }
 
-            if (token.IsCancellationRequested) return;
+
+        }
+        private void AnimateCellsColumnBlocks(ref CancellationToken token)
+        {
+            while (false == mStop)
+            {
+                for (int x = 0; x < 3; ++x)
+                {
+                    if (token.IsCancellationRequested) break;
+
+                    Thread.Sleep(234);
+                    ((ProgressSquare)tableLayoutPanel1.Controls[x + 3 * 0]).BackColor = mCellColor;
+
+                    ((ProgressSquare)tableLayoutPanel1.Controls[x + 3 * 1]).BackColor = mCellColor;
+
+
+                    ((ProgressSquare)tableLayoutPanel1.Controls[x + 3 * 2]).BackColor = mCellColor;
+
+
+                    Thread.Sleep(234);
+
+
+                }
+                ResetCells();
+
+            }
+        }
+
+        private void AnimateCellsRandom(ref CancellationToken token)
+        {
+            var r = new System.Random();
+            while (false == mStop)
+            {
+                if (token.IsCancellationRequested) break;
+                int x = r.Next(0,3);
+                int y = r.Next(0,3);
+
+                ProgressSquare cellSquare = (ProgressSquare)tableLayoutPanel1.Controls[x + 3 * y];
+                if (cellSquare.State == 0)
+                {
+                    cellSquare.FadeIn(mCellColor);
+                }
+                else
+                {
+                    cellSquare.FadeOut(mCellColor);
+                }
+
+                Thread.Sleep(88);
+            }
         }
     }
-   
 }
